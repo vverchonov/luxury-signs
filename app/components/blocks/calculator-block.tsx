0c303617e0ff9bd96003c8209d0e668d5bae0afa
Viacheslav Verchonov <vverchonov@gmail.com>
2024-04-11 23:36:27 -0400

    about done, footer done

--- app/components/blocks/calculator-block.tsx
+++ app/components/blocks/calculator-block.tsx
@@ -2,7 +2,7 @@ import { Calculator } from "../calculator/calculator";
 
 export const CalculatorBlock = () => {
   return (
-    <section className="w-full min-h-screen dark-back p-24">
+    <section className="w-full min-h-screen dark-back p-24 calculator-block">
       <div className="flex w-full flex-col">
         <h2 className="text-9xl text-white mb-12">GET QUOTE NOW</h2>
         <Calculator />
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import { WelcomeBlock } from "./components/blocks/welcome-block";
 import { WeOfferBlock } from "./components/blocks/we-offer-block";
 import { CalculatorBlock } from "./components/blocks/calculator-block";
+import { AboutUsBlock } from "./components/blocks/about-block";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <WelcomeBlock />
       <WeOfferBlock />
+      <AboutUsBlock />
       <CalculatorBlock />
     </main>
   );
