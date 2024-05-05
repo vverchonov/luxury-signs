88417489e6a2b2681068679d3a6a9a8677c2763d
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-05 13:44:10 -0400

    mob changes

--- app/components/blocks/we-offer/we-offer-item.tsx
+++ app/components/blocks/we-offer/we-offer-item.tsx
@@ -3,7 +3,7 @@ export const WeOfferItem = (props: any) => {
     <div className="flex flex-col  gap-4 offer-item relative">
       <img className="offer-img" src={props.image} />
       <div className="flex flex-col p-2">
-        <h3 className="text-2xl md:text-4xl px-[10%] font-na font-bold uppercase mt-2 mb-2 md:text-left text-center">
+        <h3 className="text-2xl md:text-4xl px-[10%] md:px-0 font-na font-bold uppercase mt-2 mb-2 md:text-left text-center">
           {props.name}
         </h3>
         <p className="font-na text-lg md:text-xl px-[10%]  md:p-0">
--- app/components/blocks/welcome-block.tsx
+++ app/components/blocks/welcome-block.tsx
@@ -4,20 +4,23 @@ export const WelcomeBlock = () => {
   return (
     <div
       id={"welcome-block"}
-      className="flex flex-col top-margin welcome-block w-full text-center items-center"
+      className="flex flex-col top-margin welcome-block w-full text-center items-center min-h-screen"
     >
-      <Wrapper>
-        <h1 className="text-9xl main-text h1-text font-cor font-semibold">
+      <Wrapper customClass={"h-full flex flex-col min-h-screen"}>
+        <h1 className="hidden md:block text-9xl main-text h1-text font-cor font-semibold">
           CUSTOM SIGNS
         </h1>
-        <p className="text-6xl text-white mt-5 font-cor font-semibold secondary-text">
+        <h1 className="block md:hidden text-9xl main-text h1-text font-cor font-semibold">
+          LUXURY DESIGN SIGNS
+        </h1>
+        <p className="md:block hidden text-6xl text-white mt-5 font-cor font-semibold secondary-text">
           LUXURY DESIGN SIGNS
         </p>
         <div className="w-full flex flex-row content-center justify-center text-center">
           <a
             href="#calculator-block"
             type="button"
-            className="estimate-button mt-20 w-fit gentle-shake font-na font-semibold text-white main-back text-2xl focus:ring-4 focus:outline-none focus:ring-white font-medium px-8 py-4 text-center transform transition duration-500 hover:scale-110"
+            className="estimate-button mb-56 w-fit gentle-shake font-na font-semibold text-white main-back text-2xl focus:ring-4 focus:outline-none focus:ring-white font-medium px-8 py-4 text-center transform transition duration-500 hover:scale-110"
           >
             ESTIMATE NOW
           </a>
