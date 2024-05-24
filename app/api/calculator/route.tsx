7f03e63ad03c283e5da3d5cc8650aa262aff6cd6
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-24 12:25:15 -0400

    1

--- app/api/calculator/route.tsx
+++ app/api/calculator/route.tsx
@@ -25,6 +25,7 @@ export async function POST(request: Request) {
 
   console.log("image name = ", imageName);
   //@ts-ignore
+
   console.log("image path = ", image.path);
 
   const transporter = nodemailer.createTransport({
